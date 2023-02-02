d1e3603572c6d1a3f79e137656231b1279df00bb
Ruslan <888lyssy@gmail.com>
2023-02-02 22:56:14 +0500

    task1

---
+++ task_1/Program.cs
@@ -0,0 +1,15 @@
+﻿int a, b;
+
+Console.WriteLine("Введите два числа : ");
+
+a = Convert.ToInt32(Console.ReadLine());
+
+b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+    Console.WriteLine( a);
+}
+else
+{
+    Console.WriteLine( b);
+}
